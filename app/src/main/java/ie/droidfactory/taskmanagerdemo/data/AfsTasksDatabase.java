d4ee4a44574f6db13f719aec15ed952b31017ba3
Nikodem Walicki <walicki@gmail.com>
2018-08-11 00:50:33 +0100

    add header to list

--- app/src/main/java/ie/droidfactory/taskmanagerdemo/data/AfsTasksDatabase.java
+++ app/src/main/java/ie/droidfactory/taskmanagerdemo/data/AfsTasksDatabase.java
@@ -74,7 +74,7 @@ public abstract class AfsTasksDatabase extends RoomDatabase {
             setDatabaseCreated();
         }
     }
-    
+
     private void setDatabaseCreated(){
         isDatabaseCreated.postValue(true);
     }
